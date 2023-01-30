d0f3c898d7feec11da037d4f98a3d37c80c69cb7
LarissaSimoes <larissacarneiro2@hotmail.com>
2023-01-30 17:35:43 -0300

    2,3

--- src/App.js
+++ src/App.js
@@ -3,6 +3,18 @@ import Card from './components/Card';
 import Form from './components/Form';
 
 class App extends React.Component {
+  // state = {
+  //   cardName: '',
+  //   cardDescription: '',
+  //   cardAttr1: 0,
+  //   cardAttr2: 0,
+  //   cardAttr3: 0,
+  //   cardImage: '',
+  //   cardRare: 'normal',
+  //   cardTrunfo: false,
+  //   hasTrunfo: false,
+  //   isSaveButtonDisabled: true,
+  // };
   render() {
     return (
       <div>
